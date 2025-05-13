b22ed30fa1762c457295c0b94f30c318e9d0a26e
amira abdulleiid <amira.abdulleiid@elev.ga.ntig.se>
2025-05-13 12:17:34 +0200

    ikujytgf

--- Projekt 1 – Vinterprojektet/Program.cs
+++ Projekt 1 – Vinterprojektet/Program.cs
@@ -1,1 +1,8 @@
-﻿
+﻿Console.WriteLine("Välkommen till mitt inventory spell");
+
+Console.Write("Ange din karaktärs namn:");
+string characterName = Console.ReadLine();
+
+Console.WriteLine("Välj kön:");
+Console.WriteLine("1. Kvinna");
+Console.WriteLine("2. Man");
