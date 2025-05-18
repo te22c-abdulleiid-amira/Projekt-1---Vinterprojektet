24279a01de8d4b076d6af657e6f6469a8165ad1f
amira abdulleiid <amira.abdulleiid@elev.ga.ntig.se>
2025-05-18 07:45:39 +0200

    hej

--- Projekt 1 – Vinterprojektet/Program.cs
+++ Projekt 1 – Vinterprojektet/Program.cs
@@ -1,11 +1,63 @@
-﻿Console.WriteLine("Välkommen till mitt inventory spell");
+﻿using System;
+using Projekt_1___Vinterprojektet;
 
-Console.Write("Ange din karaktärs namn:");
-string characterName = Console.ReadLine();
+Console.WriteLine("Välkommen till spelet, din gloriosa kung Lebron!");
+Console.Write("Skriv in namnet på din karaktär: ");
+string playerName = Console.ReadLine();
 
-Console.WriteLine("Välj kön:");
-Console.WriteLine("1. Kvinna");
-Console.WriteLine("2. Man");
+Character player;
 
+while (true)
+{
+    Console.Write("Välj kön på din karaktär (female/male): ");
+    string gender = Console.ReadLine().ToLower();
 
-// skapa enemy 
+    if (gender == "female")
+    {
+    player = new FemaleCharacter(playerName);
+    break; // break avslutar loop- eller switch-satser
+    }
+    else if (gender == "male")
+    {
+        // Byt ut mot MaleCharacter när den finns
+        player = new FemaleCharacter(playerName);
+        break;
+    }
+    else
+    {
+        Console.WriteLine("Felaktigt val, försök igen.");
+    }
+}
+
+Weapon sword = new Weapon("svärd", 10, 15);
+Potions potion = new Potions("medicin", 10, 20);
+Armor shield = new Armor("sköld", 10, 5);
+
+// lägg till items i spelarens inventory
+player.AddToInventory(sword);
+player.AddToInventory(potion);
+player.AddToInventory(shield);
+
+// visa inventory
+player.ShowInventory();
+// använd ett item (exempel: använda potion på sig själv)
+Console.WriteLine("\nAnvänder Liten hälsodryck på dig själv:");
+player.UseItem("Liten hälsodryck");
+
+// använd ett annat item (exempel: equipa svärd)
+Console.WriteLine("\nEquipa Svärd:");
+player.UseItem("Svärd");
+
+// akapa en fiende
+Enemy enemy = new Enemy("Onskefulla Fienden", 80, 8);
+
+// fienden attackerar spelaren
+enemy.Attack(player);
+
+// visa spelarens HP efter attacken
+Console.WriteLine($"{player.GetName()} har {player.GetHP()} HP kvar.");
+
+    
+Console.WriteLine("\nTryck på valfri tangent för att avsluta.");
+Console.ReadKey();
+
